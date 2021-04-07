e99b91383a8cb0276af5315a576c6cea425c31a0
Sujin <cavok0929@gmail.com>
2021-04-07 21:46:02 +0900

    CSS: Mobile Version add

--- main.js
+++ main.js
@@ -3,20 +3,14 @@
 const btn = document.querySelector(".btn");
 const backBtn = document.querySelector("#btn-back");
 const cardContainer = document.querySelector(".card__container");
-const avatar = document.querySelector("#avatar");
-const charName = document.querySelector("#char__name");
-const charAbility = document.querySelector("#char__ability");
-const charHeight = document.querySelector("#char__height");
-const charWeight = document.querySelector("#char__weight");
+const homeContainer = document.querySelector(".body__container");
 
 btn.addEventListener("click", e => {
 	e.preventDefault();
-	cardContainer.classList.add("show");
-	avatar.src = "img/pokeball.png";
-	charName.innerHTML = "<em>Loading...</em>";
-	charAbility.innerHTML = "<em>Loading...</em>";
-	charHeight.innerHTML = "<em>Loading...</em>";
-	charWeight.innerHTML = "<em>Loading...</em>";
+	cardContainer.classList.remove("invisible")
+	homeContainer.classList.add("invisible");
+
+	
 
 	const randomNumber = Math.ceil(Math.random() * 898);
 
@@ -34,5 +28,6 @@ btn.addEventListener("click", e => {
 
 backBtn.addEventListener("click", e => {
 	e.preventDefault();
-	cardContainer.classList.remove("show");
+	homeContainer.classList.remove("invisible")
+	cardContainer.classList.add("invisible");
 });
